71e90447571a9911ed8bd7b3e236158e3c155682
Kayo Stabile <kayolestabile@gmail.com>
2026-07-27 20:39:58 -0300

    feat: Implemetnado exclusão de clientes

--- clientes.py
+++ clientes.py
@@ -1,7 +1,10 @@
+import os
 from database import conexao, cursor
 
 
 def cadastrar_cliente():
+    os.system("cls")
+    print("---- Cadastro de cliente ----")
     nome = input("Nome: ")
     email = input("Email: ")
     telefone = input("Telefone: ")
@@ -15,10 +18,37 @@ def cadastrar_cliente():
 
 
 def listar_clientes():
+    os.system("cls")
     cursor.execute("SELECT * FROM clientes")
     clientes = cursor.fetchall()
+    print("---- Lista de clientes ----")
     for id_cliente, nome, email, telefone in clientes:
+        print("-" * 30)
         print(f"Id: {id_cliente}")
         print(f"Nome: {nome}")
         print(f"Email: {email}")
         print(f"Telefone: {telefone}")
+
+
+def excluir_cliente():
+    os.system("cls")
+    cursor.execute("SELECT id_cliente, nome FROM clientes")
+    clientes = cursor.fetchall()
+    for id_cliente, nome in clientes:
+        print("-" * 30)
+        print(f"Id: {id_cliente}")
+        print(f"Nome: {nome}")
+    id_excluir = input("Digite o ID do cliente que deseja excluir: ")
+    if not id_excluir.isdigit():
+        print("Digite um Id válido")
+        return
+    id_excluir = int(id_excluir)
+    sql = "DELETE FROM clientes WHERE id_cliente = %s"
+    valores = (id_excluir,)
+
+    cursor.execute(sql, valores)
+    conexao.commit()
+    if cursor.rowcount > 0:
+        print("Cliente excluido com sucesso!")
+    else:
+        print("Nenhum cliente com esse Id")
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from clientes import (cadastrar_cliente, listar_clientes)
+from clientes import (cadastrar_cliente, listar_clientes, excluir_cliente)
 
 while True:
-    print("----- MENU -----")
+    print("-------- MENU --------")
     print("1 - Cadastrar cliente")
     print("2 - Listar clientes")
-    print("3 - Sair")
+    print("3 - Excluir cliente")
+    print("4 - Sair")
+
     escolha = input("Escolha: ")
     if escolha.isdigit():
         escolha = int(escolha)
@@ -13,6 +15,8 @@ while True:
         elif escolha == 2:
             listar_clientes()
         elif escolha == 3:
+            excluir_cliente()
+        elif escolha == 4:
             break
         else:
             print("Digite um número válido")
